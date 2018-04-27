4401338c4c7668f7cefbe0d104f945034e20a32b
MonkeykingYan <monkeykingyan@gmail.com>
2018-04-27 00:48:44 -0400

    isqunique

---
+++ src/Array_String/IsUnique.java
@@ -0,0 +1,20 @@
+package Array_String;
+
+//Implement an algorithm to determine if a string has all unique characters, what if we cannot use additional data structure
+public class IsUnique {
+    public boolean checker(String s) {
+        boolean[] allchar = new boolean[128];
+        for (char c : s.toLowerCase().toCharArray()) {
+            int index = (int) c;
+            if (allchar[index]) return false;
+            allchar[index] = true;
+        }
+        return true;
+    }
+
+    public static void main(String argv[]) {
+        String t1 = "MayAn";
+        IsUnique is = new IsUnique();
+        System.out.print(is.checker(t1));
+    }
+}
