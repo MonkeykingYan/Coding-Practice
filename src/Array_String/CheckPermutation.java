9cfe5bba8483db0afe99fe15df65172d8054deb5
MonkeykingYan <monkeykingyan@gmail.com>
2018-04-27 23:32:04 -0400

    permutation

--- src/Array_String/CheckPermutation.java
+++ src/Array_String/CheckPermutation.java
@@ -1,5 +1,6 @@
 package Array_String;
 
+import java.util.Arrays;
 import java.util.HashMap;
 
 //Given 2 strings, write a method to determine if one is a permutation of the other
@@ -17,19 +18,25 @@ public class CheckPermutation {
         return true;
     }
 
-    public boolean solution1(String s1, String s2)
-    {
-        if(s1.length()!= s2.length()) return false;
-        
-        return true;
+    public boolean solution1(String s1, String s2) {
+        if (s1.length() != s2.length()) return false;
+        String sortedS1 = this.sortString(s1);
+        String sortedS2 = this.sortString(s2);
+        return sortedS1.equals(sortedS2);
+    }
+
+    public String sortString(String s) {
+        char[] content = s.toCharArray();
+        Arrays.sort(content);
+        return new String(content);
     }
 
 
     public static void main(String argv[]) {
         String t1 = "mayan";
-        String t2 = "yanmashen";
+        String t2 = "yanma";
         CheckPermutation cp = new CheckPermutation();
-        boolean res = cp.permutaion(t1, t2);
+        boolean res = cp.solution1(t1, t2);
         System.out.print(res);
     }
 }
