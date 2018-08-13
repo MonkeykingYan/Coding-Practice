430da1ad4b398666435e32159ff5ccb8ee7009a2
YanMa <monkeykingyan@gmail.com>
2018-08-13 14:16:28 -0400

    update linked list 2.3

--- src/Linked_List/DeleteMiddleNode.java
+++ src/Linked_List/DeleteMiddleNode.java
@@ -47,10 +47,14 @@ public class DeleteMiddleNode {
         ListNode node2 = new ListNode(1);
         ListNode node3 = new ListNode(3);
         ListNode node4 = new ListNode(2);
+        ListNode node5 = new ListNode(5);
+        ListNode node6 = new ListNode(6);
 
         node1.next = node2;
         node2.next = node3;
         node3.next = node4;
+        node4.next = node5;
+        node5.next = node6;
 
         DeleteMiddleNode dmid = new DeleteMiddleNode();
         dmid.deleteMiddle(node1);
