77be6074e79f6de5327fe4288cc3942e5a872ced
YanMa <monkeykingyan@gmail.com>
2018-09-12 17:21:30 -0400

    new recursive question

---
+++ src/Recursion_Dynamic/Parens.java
@@ -0,0 +1,13 @@
+package Recursion_Dynamic;
+
+/**
+ * Parens: Implement an algorithm to print all valid (e.g., properly opened and closed) combinations
+ * of n pairs of parentheses.
+ * EXAMPLE
+ * Input: 3
+ * Output: ( ( () ) ) , ( () () ) , ( () ) () , () ( () ) , () () ()
+ *
+ * https://leetcode.com/problems/generate-parentheses/description/
+ */
+public class Parens {
+}
