e5cddaf10bd3000cc2e64e13dfc738b4572345d1
YanMa <monkeykingyan@gmail.com>
2018-09-16 17:27:59 -0400

    Coins

---
+++ src/Recursion_Dynamic/Coins.java
@@ -0,0 +1,31 @@
+package Recursion_Dynamic;
+
+public class Coins {
+    public int change(int amount, int[] coins) {
+        int[] res = new int[amount + 1];
+        res[0] = 1;
+
+        for (int coinVAl : coins) {
+            for (int i = 0; i <= amount; i++) {
+                if (i >= coinVAl) {
+                    res[i] = res[i] + res[i - coinVAl];
+                }
+            }
+        }
+        printAmount(res);
+        return res[amount];
+    }
+
+    public static void printAmount(int[] amount) {
+        for (int i = 0; i < amount.length; i++) {
+            System.out.println("For amount:" + i + " The nums is:" + amount[i]);
+        }
+    }
+
+    public static void main(String[] argv) {
+        Coins c = new Coins();
+        System.out.println(c.change(5, new int[]{1, 2, 5}));
+    }
+
+
+}
