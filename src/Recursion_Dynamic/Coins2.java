7ef0e299ec38224decbe842da5c47c20982e7cbc
YanMa <monkeykingyan@gmail.com>
2018-09-17 16:00:02 -0400

    Coins2

---
+++ src/Recursion_Dynamic/Coins2.java
@@ -0,0 +1,36 @@
+package Recursion_Dynamic;
+
+
+/**
+ * You are given coins of different denominations and a total amount of money amount. Write a function to compute the fewest number of coins that you need to make up that amount. If that amount of money cannot be made up by any combination of the coins, return -1.
+ * <p>
+ * Example 1:
+ * <p>
+ * Input: coins = [1, 2, 5], amount = 11
+ * Output: 3
+ * Explanation: 11 = 5 + 5 + 1
+ * Example 2:
+ * <p>
+ * Input: coins = [2], amount = 3
+ * Output: -1
+ */
+public class Coins2 {
+    public int coinChange(int[] coins, int amount) {
+        if (amount == 0) return 0;
+        int min = amount;
+        for (int coin : coins) {
+            if (amount - coin >= 0) {
+                int temp = coinChange(coins, amount - coin);
+                if (temp + 1 < min) min = temp + 1;
+            }
+        }
+        return min;
+    }
+
+
+    public static void main(String[] argv) {
+        Coins2 c2 = new Coins2();
+        System.out.println(c2.coinChange(new int[]{1,2,5}, 11));
+
+    }
+}
---
+++ src/Test/Basic.java
@@ -0,0 +1,10 @@
+package Test;
+
+public class Basic {
+    public static void main(String[] argv)
+    {
+        int a = 6, b =12, c = 5;
+
+        System.out.println(a/b);
+    }
+}
