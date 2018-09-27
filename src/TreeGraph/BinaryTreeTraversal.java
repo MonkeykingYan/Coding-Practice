ff918404460451d927149cafc5932cedcdbbe55f
YanMa <monkeykingyan@gmail.com>
2018-09-27 17:13:33 -0400

    binary tree traversal

---
+++ src/TreeGraph/BinaryTreeTraversal.java
@@ -0,0 +1,39 @@
+package TreeGraph;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * https://leetcode.com/problems/binary-tree-preorder-traversal/
+ */
+public class BinaryTreeTraversal {
+
+    List<Integer> res = new ArrayList<>();
+
+    public List<Integer> preorderTraversal(TreeNode root) {
+        if (root != null) {
+            res.add(root.val);
+            preorderTraversal(root.left);
+            preorderTraversal(root.right);
+        }
+        return res;
+    }
+
+    public List<Integer> inorderTraversal(TreeNode root) {
+        if (root != null) {
+            inorderTraversal(root.left);
+            res.add(root.val);
+            inorderTraversal(root.right);
+        }
+        return res;
+    }
+
+    public List<Integer> postorderTraversal(TreeNode root) {
+        if (root != null) {
+            postorderTraversal(root.left);
+            postorderTraversal(root.right);
+            res.add(root.val);
+        }
+        return res;
+    }
+}
